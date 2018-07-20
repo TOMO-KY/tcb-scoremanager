7a18952ed712bb5d758f10bab10ab85ca1ef76d4
KY <goonimo@gmail.com>
2018-07-20 11:02:00 +0900

    インストール設定用メモファイルの追加 セキュリティ設定を変更

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'V2LeH63etZt2DbpKa2pXStZnZzyDax67GcWVjXP5');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '971302359810241048185498758802');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
